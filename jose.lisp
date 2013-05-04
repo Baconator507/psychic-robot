12debc5911827b3897df3f3f49007c6e960fdd1e
Baconator507 <ibarria0@gmail.com>
2013-05-04 16:10:27 -0400

    upate logic

--- jose.lisp
+++ jose.lisp
@@ -1,5 +1,25 @@
 (defun action(roundstate id)
-  (declare (ignore roundstate id)) (LIST :fold))
+  (let ((mybank (aref (holdemround-playerbanks roundstate) id)))
+    (cond ((zerop (holdemround-bet roundstate))
+           (cond ((and (< (number-of-raises roundstate) 3)
+                       (> mybank (holdemround-blind roundstate)))
+                  (LIST :raise (holdemround-blind roundstate)))
+                 ((and (< (number-of-raises roundstate) 3)
+                       (<= mybank (holdemround-blind roundstate)))
+                  (LIST :allin))
+                 ((and (= (number-of-raises roundstate) 3)
+                       (> mybank (holdemround-bet roundstate)))
+                  (LIST :call))
+                 ((and (= (number-of-raises roundstate) 3)
+                       (< mybank (holdemround-bet roundstate)))
+                  (LIST :allin))
+                 (t (LIST :fold))))
+          ((> mybank (+ (holdemround-blind roundstate) 
+                        (holdemround-bet roundstate)))
+           (LIST :raise (holdemround-blind roundstate)))
+          (t ;; can't cover the bet or the blind
+           (LIST :allin)))))
+
 
 (defparameter *jose* 
   (make-holdemagent
