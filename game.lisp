6c9384328560dcf0c523e7d0e89583d92ce00a8c
Baconator507 <ibarria0@gmail.com>
2013-05-04 02:10:36 -0400

    main program

---
+++ game.lisp
@@ -0,0 +1,7 @@
+(load "pokerlib7.lisp")
+(load "jose.lisp")
+
+(defparameter *game-players* 
+  (list *aggressive-caller-agent* *less-aggressive-caller-agent* *jose* *raiser-agent*))
+
+(holdem-game-driver *game-players* :verbose t :pauser t)
