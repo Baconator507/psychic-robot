4c5bcb9e95f1e7df33a63674e2ee127b5d38d5ba
Baconator507 <ibarria0@gmail.com>
2013-05-04 18:51:48 -0400

    test program

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ for i in range(10):
   p = subprocess.Popen("clisp game.lisp", stdout=subprocess.PIPE, shell=True)
   (output, err) = p.communicate()
   jose = output.split(':')[2].split(' ')[3]
-  if jose == '40000':
+  if (jose == '40000'):
     print "jose WINS!"
-    count += 1
-print "jose win ration: %s" % count/10
+    count = count + 1
+
+print "jose win ration: %0.2f" % ((count/10.0) * 100)
