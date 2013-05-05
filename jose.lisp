28e80044970fd957d48e3fad36afd480aaa61a98
Baconator507 <ibarria0@gmail.com>
2013-05-04 23:24:54 -0400

    informed logic

--- jose.lisp
+++ jose.lisp
@@ -3,24 +3,23 @@
   (case (list-length (holdemround-commoncards roundstate))
     (0 (return-from action (blind roundstate id)))
         ;;you can only see your two cards
-    (3 (return-from action (blind roundstate id)))
+    (3 (return-from action (informed roundstate id)))
         ;;you can see the three cards on the table and your two cards
-    (4 (return-from action (blind roundstate id)))
+    (4 (return-from action (informed roundstate id)))
         ;;you can see the four cards on the table and your two cards
-    (5 (return-from action (blind roundstate id)))))
+    (5 (return-from action (informed roundstate id)))))
         ;;you can see all five cards on the table and your two cards
 
 
 (defun informed(state id)
   (setf cards (append (my_cards state id) (holdemround-commoncards state)))
-  (setf strenght (hand-strength cards))
-  (print strenght)
-  (LIST :raise (+ (floor (* .25 (aref (holdemround-playerbanks state) id))) (holdemround-blind state))) 
-  ;;(cond
-    ;;(scenario2 action2)
-    ;;(scenario3 action3)
-  ;;)
-  ;;
+  (setf mybank (aref (holdemround-playerbanks state) id))
+  (if (or (flushp cards) (straightp cards) (straightflushp cards)) ;; if I have something good
+    (LIST :raise (+ (floor (* .25 mybank)) (holdemround-blind state))) 
+    ;; If my hand sucks:
+    (if (< (holdemround-bet state) (* .15 mybank)) ;; if bet is under 15% of my bank
+      (LIST :call) ;; I will call
+    (LIST :check))) ;; I will check
 )
 
 (defun blind(state id)
