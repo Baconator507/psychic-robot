39971aaeaf31d069e7955f739b57da3b5008b926
Baconator507 <ibarria0@gmail.com>
2013-05-04 20:35:15 -0400

    multiprocessing

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
+from multiprocessing import Pool
 import subprocess
-count = 0
-for i in range(10):
-  p = subprocess.Popen("clisp game.lisp", stdout=subprocess.PIPE, shell=True)
-  (output, err) = p.communicate()
-  jose = output.split(':')[2].split(' ')[3]
-  if (jose == '40000'):
-    print "jose WINS!"
-    count = count + 1
 
-print "jose win ration: %0.2f" % ((count/10.0) * 100)
+def clisp(x):
+  count = 0
+  for i in range(10):
+    p = subprocess.Popen("clisp game.lisp", stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    jose = output.split(':')[2].split(' ')[3]
+    if (jose == '40000'):
+      print "jose WINS!"
+      count = count + 1
+  print "jose win ration: %0.2f" % ((count/10.0) * 100)
+
+if __name__ == '__main__':
+    pool = Pool(processes=4)             
+    result = pool.map_async(clisp, range(4))
+    result.get(timeout=10000) 
