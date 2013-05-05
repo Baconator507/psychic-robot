604a25806606226420f78d20cef4b963642d9f33
Baconator507 <ibarria0@gmail.com>
2013-05-04 21:31:21 -0400

    removed conservative

--- jose.lisp
+++ jose.lisp
@@ -1,28 +1,32 @@
 (defun action(roundstate id)
+  (if (> (aref (holdemround-playerbanks roundstate) id)(* .9 (total_bank roundstate))) (LIST :allin)) ;;try to finish off players
   (case (list-length (holdemround-commoncards roundstate))
-    (0 (return-from action (conservative roundstate id)))
-    (3 (return-from action (conservative roundstate id)))
-    (4 (return-from action (conservative roundstate id)))
-    (5 (return-from action (aggresive roundstate id)))))
+    (0 (return-from action (blind roundstate id)))
+    (3 (return-from action (blind roundstate id)))
+    (4 (return-from action (blind roundstate id)))
+    (5 (return-from action (blind roundstate id)))))
 
 
-(defun aggresive(state id)
-  (if (> (aref (holdemround-playerbanks state) id)(* .9 (total_bank state))) (LIST :allin)) ;;try to finish off players
+(defun informed(state id)
   ;;(cond
     ;;(scenario2 action2)
     ;;(scenario3 action3)
   ;;)
 )
 
-(defun conservative(state id)
-    (if (> (aref (holdemround-playerbanks state) id)(* .9 (total_bank state))) (LIST :allin)) ;;try to finish off players
-    (case (random 2)
+(defun blind(state id)
+    (if (pairp (my_cards state id))
+      (case (random 2)
+        (1 (LIST :raise (floor (* .15 (total_bank state)))))
+        (2 (LIST :call))))
+    (case (random 3)
       (1 (LIST :raise (holdemround-blind state)))
-      (2 (LIST :check))))
+      (2 (LIST :check))
+      (3 (LIST :call))
+      (t (LIST :fold))))
 
 (defun total_bank(state)
   (reduce #'+ (holdemround-playerbanks state)))
-
 
 (defun my_cards(state id)
   (aref (holdemround-playercards state) id))
