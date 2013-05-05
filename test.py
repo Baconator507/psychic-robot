2c8b876ba05524e61a762306306ef29656c8035a
Ben Smith <Ben@Bens-MacBook-Pro.local>
2013-05-04 22:14:40 -0400

    better test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import subprocess
 
 def clisp(x):
   count = 0
-  for i in range(10):
+  for i in range(100):
     p = subprocess.Popen("clisp game.lisp", stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     jose = output.split(':')[2].split(' ')[3]
@@ -14,7 +14,7 @@ def clisp(x):
     else:
        print str(i) + " jose didn't win"
 
-  print "jose win ration: %0.2f" % ((count/10.0) * 100)
+  print "jose win ration: %0.2f" % ((count/100.0) * 100)
 
 if __name__ == '__main__':
     pool = Pool(processes=4)             
