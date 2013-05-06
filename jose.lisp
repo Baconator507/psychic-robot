d15e542333c5e474e40e27bd5580082319c00f5a
Baconator507 <ibarria0@gmail.com>
2013-05-05 20:40:11 -0400

    safe bets

--- jose.lisp
+++ jose.lisp
@@ -1,6 +1,6 @@
 (defun action(roundstate id)
   (if (zerop (holdemround-bet roundstate)) (LIST :check) ) 
-  (if (> (aref (holdemround-playerbanks roundstate) id)(* .9 (total_bank roundstate))) (LIST :allin)) ;;try to finish off players
+  ;;(if (> (aref (holdemround-playerbanks roundstate) id)(* .9 (total_bank roundstate))) (LIST :allin)) ;;try to finish off players
   (case (list-length (holdemround-commoncards roundstate))
     (0 (return-from action (blind roundstate id)))
         ;;you can only see your two cards
@@ -15,9 +15,9 @@
 (defun informed(state id)
   (let ((cards (append (my_cards state id) (holdemround-commoncards state)))
         (mybank (aref (holdemround-playerbanks state) id)))
-  (if (or (flushp cards) (straightp cards) (straightflushp cards) (fullhousep cards) (fourkindp cards) ) ;; if I have something good
-    (LIST :raise (+ (floor (* .45 mybank)) (holdemround-blind state))) 
-    (if (and (or (pairp cards) (tripsp cards) (twopairp cards) (< 5 (list-length (holdemround-commoncards state)))) (< (holdemround-bet state) ( * 0.15 mybank)))  ;; not so good
+  (if (or (flushp cards) (straightp cards) (tripsp cards) (straightflushp cards) (fullhousep cards) (fourkindp cards) ) ;; if I have something good
+    (LIST :raise (+ (floor (* .35 mybank)) (holdemround-blind state))) 
+    (if (and (or (pairp cards) (twopairp cards) (< 5 (list-length (holdemround-commoncards state)))) (< (holdemround-bet state) ( * 0.15 mybank)))  ;; not so good
       (case (random 3)
         (0 (LIST :raise (+ (floor (* .10 mybank)) (holdemround-blind state))))
         (t (LIST :call)))
@@ -29,14 +29,15 @@
 ))
 
 (defun blind(state id)
+  (let ((mybank (aref (holdemround-playerbanks state) id)))
   (if (> (aref (holdemround-playerbanks state) id) (holdemround-blind state)) (LIST :allin))
     (if (pairp (my_cards state id))
       (case (random 2)
         (0 (LIST :raise (holdemround-blind state)))
         (1 (LIST :call))))
-    (case (random 3)
-      (0 (LIST :call))
-      (t (LIST :check))))
+    (if (< (holdemround-bet state) (* 0.15 mybank))
+      (LIST :call)
+      (LIST :check))))
 
 (defun total_bank(state)
   (reduce #'+ (holdemround-playerbanks state)))
